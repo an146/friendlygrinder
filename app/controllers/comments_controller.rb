4f3048fed4077e88d43bd028b1eb64f485b0120d
Anton Novikov <an146@ya.ru>
2010-04-11 23:21:50 +0400

    simple captcha

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,10 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(params[:comment])
-
-    if @comment.save
+    if params[:captcha] != '444'
+      flash[:notice] = "Error creating comment: wrong captcha"
+      redirect_to(@comment.post)
+    elsif @comment.save
       flash[:notice] = 'Comment was successfully created.'
       redirect_to(@comment.post)
     else
